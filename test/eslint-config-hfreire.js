b398764bd72f44272e72942d07887f766ebc427b
Hugo Freire <hfreire@exec.sh>
2017-06-08 19:55:38 +0200

    test: add test

---
+++ test/eslint-config-hfreire.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+describe('ESlint configuration', () => {
+  let subject
+
+  describe('when exporting', () => {
+    beforeEach(() => {
+      subject = require('../src/eslint-config-hfreire')
+    })
+
+    it('should export configuration extending standard', () => {
+      subject.should.have.property('extends')
+      subject.extends.should.include('standard')
+    })
+
+    it('should export configuration using mocha plugin', () => {
+      subject.should.have.property('plugins')
+      subject.plugins.should.include('mocha')
+    })
+  })
+})
