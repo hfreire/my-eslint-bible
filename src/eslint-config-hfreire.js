05c58ae2b0a0cdf2d27a1a1419d7b6d10fc1bea8
Hugo Freire <hfreire@exec.sh>
2020-03-10 22:31:54 +0100

    fix: tweak rules

--- src/eslint-config-hfreire.js
+++ src/eslint-config-hfreire.js
@@ -27,16 +27,19 @@ module.exports = {
       'LabeledStatement',
       'WithStatement'
     ],
-
-    'object-curly-spacing': ['error', 'always'],
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'global-require': 'off',
+    'object-curly-spacing': [ 'error', 'always' ],
     'space-before-function-paren': 'error',
     'import/prefer-default-export': 'off',
+    'mport/no-extraneous-dependencies': 'off',
     'promise/always-return': 'off',
     'promise/no-return-wrap': 'error',
     'promise/param-names': 'error',
     'promise/catch-or-return': 'error',
     'promise/no-callback-in-promise': 'error',
-    'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+    'unicorn/filename-case': [ 'error', { case: 'kebabCase' } ],
     'unicorn/no-abusive-eslint-disable': 'error',
     'unicorn/no-process-exit': 'error',
     'unicorn/no-new-buffer': 'error'
